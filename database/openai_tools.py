a1abcbad7ce6bb929005d796d5fe8597e6d5c728
hongyuchen2andrew <151486368+hongyuchen2andrew@users.noreply.github.com>
2024-05-06 20:04:58 -0700

    Update openai_tools.py

--- database/openai_tools.py
+++ database/openai_tools.py
@@ -8,7 +8,7 @@ from openai import OpenAI
 api_key = os.environ['OPENAI_API_KEY']
 client = OpenAI()
 
-def chatGPT(user_prompt:str, system_prompt: str, temperature: float, document:str)->str:
+def chatGPT(user_prompt:str, system_prompt: str, temperature: float, max_tokens: int, document:str='')->str:
     completion = openai.chat.completions.create(
     model="gpt-4",
     messages=[
@@ -16,11 +16,53 @@ def chatGPT(user_prompt:str, system_prompt: str, temperature: float, document:st
         {"role": "system", "content": f"This is the document: {document}"},
         {"role": "user", "content": f"{user_prompt}"}
     ],
-    temperature = temperature
+    temperature = temperature,
+    max_tokens = max_tokens,
     )
     response = completion.choices[0].message.content
     return response
 
+def summarization(document, length, format, system_prompt):
+    temperature = 1
+    if length == 'short':
+        max_tokens = 50
+        system_prompt += ' Your answer should be breif enough to guarantee that the output tokens are less than 50.'
+    elif length == 'medium':
+        max_tokens = 100
+        system_prompt += ' Your answer should be in moderate length with the output tokens less than 100 but larger than 50.'
+    elif length == 'long':
+        max_tokens = 250
+        system_prompt += ' Your answer should be long enough with the output tokens less than 250 but larger than 100.'
+    if format == 'paragraph':
+        answer_format = 'Your answer should be writen in paragraphs. You can not use bullet point.'
+    elif format == 'bullet':
+        answer_format = '''
+        Your answer should be listed bullet point like this 
+        'Summary:
+        •<Write your summary here>
+        •<Write your summary here>'
+        '''
+    user_prompt = answer_format
+    summary = chatGPT(user_prompt, system_prompt, temperature, max_tokens)
+    return summary
+# do summarization
+def summary_assistant(document_list, length, format):
+    summary_list = []
+    for i, doc in enumerate(document_list):
+        system_prompt1 = f'''
+            This is the content from a webpage {doc}.
+            Please do summarization for the content of the current webpage.
+            '''
+        summary = summarization(doc, length, format, system_prompt1)
+        summary_list.append(f'The summary of webpage {i} is: ' + summary)
+    summaries =  '\n'.join(summary_list)
+    system_prompt2 = f'''
+    These are the summaries from several webpages: {summaries}.
+    Please combine these summaries together and summarize all if them.
+    '''
+    final_summary = summarization(doc, length, format, system_prompt2)
+    return final_summary
+    
 # Function to encode the image
 def encode_image(image_path):
   with open(image_path, "rb") as image_file:
